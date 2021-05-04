ece85c2767c6289895ea5d9e9dd810984831e4a1
Jhones Sena <jhonessena@yahoo.com.br>
2021-05-04 09:35:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+ <form class="form-horizontal" method="post"  action="convert.php" name="inserir" enctype="multipart/form-data">
+  <input type="file" class="form-control" name="arquivo" required=""accept="image/*;capture=camera" />
+  <button class="btn  btn-danger btn-sm "> Inserir </button>
+</form>
